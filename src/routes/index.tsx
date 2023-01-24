83babd2c99194b05cde5d72cdba809da3e97c2ac
Neil Ramaswamy <neil@ramaswamy.org>
2023-01-24 01:05:54 -0500

    add more interesting example to front page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,12 +8,12 @@ import { SystemSerialization } from "~/backend/types";
 
 export default function Home() {
     const history = generateHistoryFromString(`
-    ----[A:x<-  1]---------------------------------------
-    ----------------[B:x->  1]---[C:x<-  2]---[D:x<-  3]-
+    ----[A:x<-  1]-------------------------------------------[E:x->  2]--
+    ----------------[B:x->  1]---[C:x<-  2]---[D:x<-  3]-----------------
     `);
 
-    const abcd1 = generateSerialization(history, "A B C D");
-    const abcd2 = generateSerialization(history, "A B C D");
+    const abcd1 = generateSerialization(history, "A B C D E");
+    const abcd2 = generateSerialization(history, "A B C D E");
 
     const [serial, setSerial] = createSignal<SystemSerialization>({ 0: abcd1, 1: abcd2 });
 
