54c5d0a450724aa64a28c4965a5aeb82ca593c58
Neil Ramaswamy <neil.ramaswamy@databricks.com>
2024-05-11 22:17:25 -0400

    Maybe fix Vercel 404's?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import devtools from "solid-devtools/vite";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
-    plugins: [devtools({ autoname: true }), solid({ ssr: false })],
+    plugins: [solid({ adapter: vercel() })],
 });
